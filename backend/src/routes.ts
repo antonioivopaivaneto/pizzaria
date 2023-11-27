ed7f811c6279048bca6b3db2b935f3cf18c1709e
junior <antonioivo.3@gmail.com>
2023-11-27 10:59:25 -0300

    Category Create

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,11 +4,17 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticate } from './middlewares/isAuthenticate';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 const router = Router();
+
+// --category user
 
 router.post('/users',new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me',isAuthenticate, new DetailUserController().handle)
+
+// --category routes
+router.post('/category',isAuthenticate,new CreateCategoryController().handle)
 
 export {router}
